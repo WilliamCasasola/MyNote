51a4ff6ca2a088e07168ec34d0a841dde767be34
William Casasola <wacvillalobos@hotmail.es>
2023-11-03 08:28:03 -0600

    Add Feature Envy

--- MyNote/Helpers/MeetingBooker.cs
+++ MyNote/Helpers/MeetingBooker.cs
@@ -3,8 +3,26 @@ namespace MyNote.Helpers
 {
 	public class MeetingBooker
 	{
+		private ParticipantInvitation invitation;
+
 		public MeetingBooker()
 		{
+			invitation = new ParticipantInvitation();
+		}
+
+		public void CreateInvitation(string title, int participantNumbers,
+			int numberTables,
+			DateTime date)
+		{
+			invitation.SetDate(date);
+			invitation.SetNumberOfParticipants(participantNumbers);
+			invitation.SetNumberOfTables(numberTables);
+			invitation.SetTitle(title);
+		}
+
+		public void ShowInvitation()
+		{
+			invitation.GenerateConfirmation();
 		}
 
 		private void AvoidMultipleMeetingOneDay()
--- MyNote/Helpers/ParticipantInvitation.cs
+++ MyNote/Helpers/ParticipantInvitation.cs
@@ -2,10 +2,58 @@
 namespace MyNote.Helpers
 {
 	public class ParticipantInvitation
-	{
-		public ParticipantInvitation()
+	{		
+		private Int32 _numberOfParticipants;
+		private Int32 _numberOfTables;
+		private DateTime _date;
+		private string _title;
+
+		public DateTime GetDate()
 		{
+			return _date;
 		}
-	}
+
+		public void SetDate(DateTime date)
+		{
+			this._date = date;
+		}
+
+        public Int32 GetNumberOfParticipants()
+        {
+            return _numberOfParticipants;
+        }
+
+        public void SetNumberOfParticipants(Int32 numberOfParticipants)
+        {
+            this._numberOfParticipants = numberOfParticipants;
+        }
+
+        public Int32 GetNumberOfTables()
+        {
+            return _numberOfTables;
+        }
+
+        public void SetNumberOfTables(Int32 numberOfTables)
+        {
+            this._numberOfTables = numberOfTables;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        public void SetTitle(string title)
+        {
+            this._title = title;
+        }
+
+        public string GenerateConfirmation()
+        {
+            return "The date " + _date + " there would be the meeting " + title +
+                " for " + _numberOfParticipants + "participants and ther will be "
+                + _numberOfTables + " places reserved for you";
+        }
+    }
 }
 
